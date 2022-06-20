2df53e954ec8d90f8426637b15839e6365ad54a6
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2022-06-20 11:30:38 -0500

    Update plane/api/endpoints/avatars.py

--- plane/api/endpoints/avatars.py
+++ plane/api/endpoints/avatars.py
@@ -5,7 +5,7 @@
 # You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
-"""The implementations for the `avatars` endpoint."""
+"""Implementations for the `avatars` endpoint."""
 
 from __future__ import annotations
 
