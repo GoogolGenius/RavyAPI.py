471b6cf72796eb0398d81b95f64e2e23b966ae6b
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2022-06-20 11:32:52 -0500

    Update plane/api/endpoints/urls.py

--- plane/api/endpoints/urls.py
+++ plane/api/endpoints/urls.py
@@ -119,7 +119,7 @@ class URLs(HTTPAwareEndpoint):
         Raises
         ------
         TypeError
-            If any parameters are invalid type.
+            If any parameters are of invalid types.
         ValueError
             If any parameters are invalid value.
         """
